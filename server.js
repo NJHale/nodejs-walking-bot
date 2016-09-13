ceb1e400980fd9b101377e7427615e972e1a75c9
Nicholas Hale <4217653@ups.com>
2016-09-13 13:55:49 -0400

    12 factorized for openshift

--- server.js
+++ server.js
@@ -7,29 +7,29 @@ var mqtt = require('mqtt');
 var Package = require('./models/package.js').Package;
 
 // Collect relative environment variables
-const MQTT_ADDR = 'localhost';//'localhost';//process.env.mqtt_addr;
-const MQTT_TOPIC = 'pkgTopic';//'outTopic';//process.env.mqtt_topic;
+const MQTT_PKG_HOST = process.env.MQTT_PKG_HOST || 'localhost';
+const MQTT_PKG_TOPIC = process.env.MQTT_PKG_TOPIC || 'pkgTopic';
 
-const PKG_UNIT = .0001;//process.env.pkg_unit;
-const PKG_INTERVAL = 500;//process.env.pkg_interval;
+const PKG_UNIT = Math.random() * (.049 - .001) + .001;//process.env.pkg_unit; [.001, .05]
+const PKG_INTERVAL = Math.floor(Math.random() * (2500 - 250) + 250);//process.env.pkg_interval;
 const PKG_ORIGIN = [41.071350, -74.112442, 0];//process.env.pkg_origin;
 
 // Connect to the MQTT broker
-var client = mqtt.connect(`tcp://${MQTT_ADDR}`);// ec2-54-227-228-246.compute-1.amazonaws.com
+var client = mqtt.connect(`tcp://${MQTT_PKG_HOST}`);// ec2-54-227-228-246.compute-1.amazonaws.com
 
 // Listen for connect event and send a hello world to the topic
 client.on('connect', () => {
     console.log('Hello World!');
     try {
         console.log('connecting!');
-        client.publish(MQTT_TOPIC, 'Hello MQTT - I\'m the nodejs-walking-bot!');
+        client.publish(MQTT_PKG_TOPIC, 'Hello MQTT - I\'m the nodejs-walking-bot!');
     } catch (e) {
         console.log(`problem detected in connect... ${e}`);
     }
 });
 
 // Instantiate a Package at the given origin position
-var origin = new Package (0, PKG_ORIGIN, [0, 0, 0], [0, 0, 0], 0, (new Date()).valueOf());
+var origin = new Package (Math.floor(Math.random() * 100), PKG_ORIGIN, [0, 0, 0], [0, 0, 0], 0, (new Date()).valueOf());
 // Create a msg to start the child thread
 var startMsg = `start|${JSON.stringify(origin)}|${PKG_UNIT}|${PKG_INTERVAL}`;
 console.log(startMsg);
@@ -39,8 +39,8 @@ var fork = cp.fork('./worker/walker').on('message', (msg) => {
   //var next = JSON.parse(msg);
   // {"gps_pkg":"41.071350,-74.112442,4566.6,34,4"}
   try {
-    console.log(`publishing to ${MQTT_TOPIC}` + msg);
-    client.publish(MQTT_TOPIC, msg);
+    console.log(`publishing to ${MQTT_PKG_TOPIC}` + msg);
+    client.publish(MQTT_PKG_TOPIC, msg);
     console.log('outTopic published!');
   } catch (err) {
     console.log(`An error occurred when attempting to public to outTopic: ${err}`);
